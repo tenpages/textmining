d4c104c2a5c3910814c6539d5bed167183e1a80d
Shuo <tenpages66@163.com>
2017-11-08 14:04:25 -0500

    remove trash

--- skip_gram/tokenize.py
+++
@@ -1,7 +0,0 @@
-#from vectorize import tokenizeTweets
-import vectorize
-
-if __name__=="__main__":
-	infile = "tweet_by_ID_06_11_2017__10_30_59.txt.text"
-	outfile = "tweet_by_ID_06_11_2017__10_30_59.txt.tknz"
-	vectorize.tokenizeTweets(infile, outfile)
